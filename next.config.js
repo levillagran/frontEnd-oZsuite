1623f9818e27828575c1f5977c07b2145e89b0ab
lenin pancho <leninsasos@hotmail.com>
2026-02-09 20:56:08 +1100

    fix(vercel): use default .next output directory

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const API_URL = process.env.API_URL || 'http://localhost:8080'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: '.next-dev2',
   async rewrites() {
     return [
       {
